1fcd7109dd949b22607bf0b8240001a7baacebef
dazzlingeffy <irusya9@mail.ru>
2021-05-26 19:41:19 +0300

    feat: PlayersSeparator done

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,27 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    NSInteger res = 0;
+    BOOL iLessJ, jLessK, asc, iGreatJ, jGreatK, desc;
+    
+    for (int i = 0; i < ratingArray.count; i++) {
+        for (int j = i + 1; j < ratingArray.count; j++) {
+            for (int k = j + 1; k < ratingArray.count; k++) {
+                iLessJ = [ratingArray[i] intValue] < [ratingArray[j] intValue];
+                jLessK = [ratingArray[j] intValue] < [ratingArray[k] intValue];
+                asc = iLessJ && jLessK;
+                iGreatJ = [ratingArray[i] intValue] > [ratingArray[j] intValue];
+                jGreatK = [ratingArray[j] intValue] > [ratingArray[k] intValue];
+                desc = iGreatJ && jGreatK;
+                
+                if (asc || desc) {
+                    res++;
+                }
+            }
+        }
+    }
+    NSLog(@"input: %@\nres: %ld", ratingArray, (long)res);
+    return res;
 }
 
 @end
